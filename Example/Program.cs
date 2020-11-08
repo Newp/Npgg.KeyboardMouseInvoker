ccfd44895f4301765df5c0e25383666373341dc2
Newp <mynewp@gmail.com>
2020-11-08 15:42:11 +0900

    example 추가

---
+++ Example/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Npgg.VirtualKeyboard;
+
+namespace Example
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //
+
+            //await VirtualKeyboard.MultiKeyPress(30, VirtualKeys.LeftWindows, VirtualKeys.E);
+
+            await VirtualKeyboard.MultiKeyPress(30, VirtualKeys.LeftWindows, VirtualKeys.E);
+
+            Console.WriteLine("Hello World!");
+        }
+        
+        static async Task AltTab()=> await VirtualKeyboard.MultiKeyPress(30, VirtualKeys.Alt, VirtualKeys.Tab);
+
+        static async Task Window_E()=> await VirtualKeyboard.MultiKeyPress(30, VirtualKeys.LeftWindows, VirtualKeys.E);
+
+    }
+}
---
+++ src/Npgg.VirtualKeyboard/VirtualKeyboard.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Npgg.VirtualKeyboard
+{
+
+    public enum KeyboardAction : uint
+    {
+        KeyDown = 0x00,
+        KeyUp = 0x02,
+    }
+
+    
+
+    public static class VirtualKeyboard
+    {
+
+        [DllImport("user32.dll")]
+        static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, int dwExtraInfo);
+
+        public static async Task KeyPress(ushort inputTerm, params VirtualKeys[] keys)
+        {
+            foreach (var key in keys)
+            {
+                KeyDown(key);
+                await Task.Delay(inputTerm);
+                KeyUp(key);
+            }
+        }
+
+        public static async Task MultiKeyPress(ushort inputTerm, params VirtualKeys[] keys)
+        {
+            foreach (var key in keys)
+            {
+                KeyDown(key);
+                await Task.Delay(inputTerm);
+            }
+
+
+            foreach (var key in keys.Reverse())
+            {
+                KeyUp(key);
+                await Task.Delay(inputTerm);
+            }
+        }
+
+
+        public static void KeyDown(VirtualKeys key)
+        {
+            keybd_event((byte)key, 0, (uint)KeyboardAction.KeyDown, 0);
+        }
+
+        public static void KeyUp(VirtualKeys key)
+        {
+            keybd_event((byte)key, 0, (uint)KeyboardAction.KeyUp, 0);
+        }
+
+    }
+}
